2e21dc873ee8521ccd39a88f095b78e2db945251
Lane Brooks <dirjud@gmail.com>
2016-06-01 15:40:24 -0600

    Added IOBUF.v model

---
+++ IOBUF.v
@@ -0,0 +1,19 @@
+module IOBUF (O, IO, I, T);
+
+    parameter CAPACITANCE = "DONT_CARE";
+    parameter integer DRIVE = 12;
+    parameter IBUF_DELAY_VALUE = "0";
+    parameter IBUF_LOW_PWR = "TRUE";
+    parameter IFD_DELAY_VALUE = "AUTO";
+    parameter IOSTANDARD = "DEFAULT";
+    parameter SLEW = "SLOW";
+
+    output O;
+    inout  IO;
+    input  I, T;
+
+   assign IO = T ? I : 1'bz;
+   assign O = IO;
+    
+endmodule
+
